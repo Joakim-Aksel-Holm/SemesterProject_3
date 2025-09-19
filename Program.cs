024904e8c1076cbf72d06c00bfcd8a190bd205a7
niklas-hakmann <nikha22@student.sdu.dk>
2025-09-19 11:50:39 +0200

    login page

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+var client = new OpcClient("opc.tcp://127.0.0.1:4840");
+client.Connect();
+
+var value = client.ReadNode("ns=2;s=Machine/Speed");
+Console.WriteLine(value.Value);
+
+client.Disconnect();
