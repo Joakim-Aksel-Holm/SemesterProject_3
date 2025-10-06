338d8f0284b05226a3161c0d96d62f2ca84b4f83
Abdullah Sharba <absha24@student.sdu.dk>
2025-10-06 14:11:22 +0200

    db-connection + Test class in Program.cs.
    though. further action should be taken for the user credentials.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -2,10 +2,32 @@
 using Opc.UaFx;
 using Opc.UaFx.Client;
 
-var client = new OpcClient("opc.tcp://127.0.0.1:4840");
-client.Connect();
 
-var value = client.ReadNode("ns=2;s=Machine/Speed");
-Console.WriteLine(value.Value);
 
-client.Disconnect();
+class Program
+{
+    static void Main()
+    {
+        // Client connection
+        
+        
+        
+        // database connection test
+        
+        try
+        {
+            using var db = new DatabaseConnection();
+            using var conn = db.GetConnection();
+
+            using var cmd = new NpgsqlCommand("SELECT version();", conn);
+            var version = cmd.ExecuteScalar();
+
+            Console.WriteLine($"✅ Connected successfully! PostgreSQL version: {version}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"❌ Connection failed: {ex.Message}");
+        }
+    }
+    
+    }
