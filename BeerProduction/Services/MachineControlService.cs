7535edd85135034fe3928da4b44fc4d7750e0bec
niklas-hakmann <nikha22@student.sdu.dk>
2025-10-31 12:08:27 +0100

    Can start machine through program.cs now

--- BeerProduction/Services/MachineControlService.cs
+++ BeerProduction/Services/MachineControlService.cs
@@ -16,43 +16,51 @@ public class MachineControlService
         MachineControl = machineControl;
     }
 
-
+    public void SetChangeRequestTrue()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CmdChangeRequest", true);
+    }
     // methods
     public int GetStatus()
     {
         int status = MachineControl.Client.ReadNode("ns=6;s=::Program:Cube.Status.StateCurrent").As<int>(); // Reading the current status.
         return status;
     }
-    
+
     // Basic method to Reset machine by directly writing the Reset command.
     public void ResetCommand()
     {
-        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",1);
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd", 1);
+        SetChangeRequestTrue();
     }
 
     // Basic method to start machine by directly writing the start command.
 
     public void StartCommand()
     {
-        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",2);
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd", 2);
+        SetChangeRequestTrue();
     }
 
-     // Basic method to stop machine by directly writing the stop command.
-     public void StopCommand()
+    // Basic method to stop machine by directly writing the stop command.
+    public void StopCommand()
     {
-        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",3);
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd", 3);
+        SetChangeRequestTrue();
     }
 
-     // Basic method to Abort machine by directly writing the Abort command.
-     public void AbortCommand()
+    // Basic method to Abort machine by directly writing the Abort command.
+    public void AbortCommand()
     {
-        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",4);
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd", 4);
+        SetChangeRequestTrue();
     }
-    
+
     // Basic method to Clear machine by directly writing the Clear command.
     public void ClearCommand()
     {
-        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",5);
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd", 5);
+        SetChangeRequestTrue();
     }
 
 
@@ -68,7 +76,7 @@ public class MachineControlService
             return;
         }
 
-        else if ((statusVal == 2))
+        else if (statusVal == 2)
         {
             ResetCommand();
             Thread.Sleep(1000); // wait one second to make sure that the command is recieved. 
@@ -77,6 +85,10 @@ public class MachineControlService
         else if (statusVal == 3)
         {
             return;
+        }
+        else if (statusVal == 4)
+        {
+            StartCommand();
         }
         else if (statusVal == 5)
         {
@@ -94,7 +106,7 @@ public class MachineControlService
             Thread.Sleep(1000);
             ResetCommand();
             Thread.Sleep(1000);
-            StartMachine();
+            StartCommand();
         }
         else if (statusVal == 11)
         {
@@ -102,19 +114,19 @@ public class MachineControlService
         }
         else if (statusVal == 17)
         {
-             ResetCommand();
-             Thread.Sleep(1000);
-             StartMachine();
+            ResetCommand();
+            Thread.Sleep(1000);
+            StartCommand();
 
         }
-        else 
+        else
         {
             Thread.Sleep(2000);
             StartMachine();
         }
-}
+    }
 
-    public void StopMachine() 
+    public void StopMachine()
     {
         int statusVal = MachineControl.Client.ReadNode("ns=6;s=::Program:Cube.Status.StateCurrent").As<int>();
 
@@ -122,10 +134,10 @@ public class MachineControlService
         {
             return;
         }
-        else 
+        else
         {
             StopCommand();
         }
-        
+
     }
- }
+}
