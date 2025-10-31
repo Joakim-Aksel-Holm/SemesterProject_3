037c4e3f1f9624163932752a4d1c07691dbb8d9d
AndreasLC <andreaslykkecaspers@gmail.com>
2025-10-31 11:31:03 +0100

    added method for starting and stopping the machine (algoritme)

--- BeerProduction/Services/MachineControlService.cs
+++ BeerProduction/Services/MachineControlService.cs
@@ -1,5 +1,7 @@
 using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc;
+using UaFx;
+using UaFx.Client;
 
 public class MachineControlService
 {
@@ -19,8 +21,41 @@ public class MachineControlService
         int status = MachineControl.Client.ReadNode("ns=6;s=::Program:Cube.Status.StateCurrent").As<int>();
         return status;
     }
+    
+    // Basic method to Reset machine by directly writing the Reset command.
+    public void ResetCommand()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",1);
+    }
 
-    // lav metode, som resest maskinen
+    // Basic method to start machine by directly writing the start command.
+
+    public void StartCommand()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",2);
+    }
+
+     // Basic method to stop machine by directly writing the stop command.
+     public void StopCommand()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",3);
+    }
+
+     // Basic method to Abort machine by directly writing the Abort command.
+     public void AbortCommand()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",4);
+    }
+    
+    // Basic method to Clear machine by directly writing the Clear command.
+    public void ClearCommand()
+    {
+        MachineControl.Client.WriteNode("ns=6;s=::Program:Cube.Command.CntrlCmd",5);
+    }
+
+
+
+
 
     public void StartMachine() //prove of concept refactor
     {
@@ -66,10 +101,51 @@ public class MachineControlService
             Thread.Sleep(2000);
             StartMachine();
         }
+}
 
+    public void StopMachine() 
+    {
+        int statusVal = MachineControl.Client.ReadNode("ns=6;s=::Program:Cube.Status.StateCurrent").As<int>();
 
-       
-         
+        if (statusVal == 0)
+        {
+            return;
+        }
 
+        else if ((statusVal == 2))
+        {
+            // send komando, som resetter maskinen
+            // send komando, som starter maskinen.
+        }
+        else if (statusVal == 3)
+        {
+            return;
+        }
+        else if (statusVal == 5)
+        {
+            return;
+            // check de steps der skal til for at starte igen.
+        }
+        else if (statusVal == 6)
+        {
+            return;
+        }
+        else if (statusVal == 9)
+        {
+            // find de forskellige steps
+        }
+        else if (statusVal == 11)
+        {
+            // find de forskellige steps
+        }
+        else if (statusVal == 17)
+        {
+            // find the steps
+        }
+        else 
+        {
+            Thread.Sleep(2000);
+            StartMachine();
+        }
     }
  }
