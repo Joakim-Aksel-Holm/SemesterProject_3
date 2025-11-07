ce8f75a104b144153ccca79e57dcf5cd329c1fe4
niklas-hakmann <nikha22@student.sdu.dk>
2025-11-07 10:34:34 +0100

    machinecontrol singleton again

---
+++ BeerProduction/Components/Model/MachineControlSingletons/MachineControl_01.cs
@@ -0,0 +1,43 @@
+using System.Reflection.PortableExecutable;
+using Opc.Ua;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+public class MachineControl_01
+{
+
+    private static MachineControl_01 _instance = new(1, "opc.tcp://127.0.0.1:4840");
+    public float Tempurature { get; }
+    public int BatchId { get; }
+    public int Speed { get; }
+    public bool AcceptedProduct { get; }
+    public int QuantityAcceptedProduct { get; }
+
+    public int status { get; }
+
+    public string MachineUrl { get; }
+
+    public int MachineId { get; }
+
+    public OpcClient Client { get; set; }
+
+
+    //Constructor 
+    private MachineControl_01(int machineId, string machineUrl)
+    {
+        MachineId = machineId;
+        MachineUrl = machineUrl;
+        Client = new OpcClient(machineUrl);
+    }
+
+    public static MachineControl_01 Instance => _instance;
+
+    public void Connect()
+    {
+        if (Client.State != OpcClientState.Connected)
+        {
+            Client.Connect();
+        }
+    }
+
+}
