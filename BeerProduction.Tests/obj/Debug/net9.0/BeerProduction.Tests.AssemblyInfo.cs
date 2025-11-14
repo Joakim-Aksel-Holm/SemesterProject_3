ffb9626c98fea2711cc1e5dd7dab405bc7b71a0f
Ltechsystem <joachimlow02@gmail.com>
2025-11-14 09:06:28 +0100

    bug

--- BeerProduction.Tests/obj/Debug/net9.0/BeerProduction.Tests.AssemblyInfo.cs
+++ BeerProduction.Tests/obj/Debug/net9.0/BeerProduction.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeerProduction.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d35a7a8d613284429d01a607f506c9fc12c17dab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fea9b53f1c18f9acb0fcdaff0b4e8472ff3033d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeerProduction.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeerProduction.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
