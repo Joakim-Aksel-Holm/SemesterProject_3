2e2ca4ffc850b605e0d6f62bcc0dbf54814baff0
Ltechsystem <joachimlow02@gmail.com>
2025-11-30 16:16:15 +0100

    - Added comments to AuthenticationStateService.cs - Removed unneccessary components - Restructured NavMenu.razor and updated redirection

--- BeerProduction/Services/AuthenticationStateService.cs
+++ BeerProduction/Services/AuthenticationStateService.cs
@@ -5,13 +5,26 @@ namespace BeerProduction.Services
 {
     public class AuthenticationStateService : AuthenticationStateProvider
     {
+        /// <summary>
+        /// Constructs the current user (static)
+        /// </summary>
         private static ClaimsPrincipal _currentUser = new(new ClaimsIdentity());
 
+        // =========================================================================
+        // Authentication methods (async)
+        // =========================================================================
+        
+        /// <summary>
+        /// Gets the current user authentication state
+        /// </summary>
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             return Task.FromResult(new AuthenticationState(_currentUser));
         }
-
+        
+        /// <summary>
+        /// Inputs the user credentials and claims an identity
+        /// </summary>
         public Task LoginAsync(string userId, string username, string roleName)
         {
             var identity = new ClaimsIdentity(new[]
@@ -27,26 +40,40 @@ namespace BeerProduction.Services
             
             return Task.CompletedTask;
         }
-
+        
+        /// <summary>
+        /// Returns the current user to an unauthenticated state by nullifying the identity
+        /// </summary>
         public Task LogoutAsync()
         {
             _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
             
             NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
             
+            Console.WriteLine("Successfully logged out");
+            
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Gets the current user's id through the claim 'NameIdentifier'
+        /// </summary>
         public string? GetUserId()
         {
             return _currentUser.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
 
+        /// <summary>
+        /// Gets the current user's username through the claim 'Identity'
+        /// </summary>
         public string? GetUsername()
         {
             return _currentUser.Identity?.Name;
         }
 
+        /// <summary>
+        /// Gets the current user's role through the claim 'Role'
+        /// </summary>
         public string? GetRole()
         {
             return _currentUser.FindFirst(ClaimTypes.Role)?.Value;
