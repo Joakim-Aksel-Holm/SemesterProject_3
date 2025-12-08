1c7646861a5446cf475bf48f8667e1137c66ee4b
niklas-hakmann <nikha22@student.sdu.dk>
2025-12-08 09:15:38 +0100

    Data validation in operator page

--- BeerProduction/Components/Model/BatchForm.cs
+++ BeerProduction/Components/Model/BatchForm.cs
@@ -9,9 +9,11 @@ public class BatchForm
     /// Attributes for batch form
     /// </summary>
     [Required(ErrorMessage = "Amount is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0")]
     public int Size { get; set; }
 
     [Required(ErrorMessage = "Speed is required")]
+    [Range(1, 600, ErrorMessage = "Amount must be between 1 - 600")]
     public float Speed { get; set; }
     
     [Required(ErrorMessage = "Must select beer type")]
